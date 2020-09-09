d7151bc0666ac779b0c405447b09ab231d997774
Z3NJIN <marlondvg@gmail.com>
2020-09-08 22:45:57 -0500

    Añadida funcionalidad para registrar un álbum compartido con un usuario

--- wchallenge/src/main/java/com/wolox/api/WchallengeApplication.java
+++ wchallenge/src/main/java/com/wolox/api/WchallengeApplication.java
@@ -1,7 +1,9 @@
 package com.wolox.api;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -150,6 +154,17 @@ public class WchallengeApplication {
 		}
 				
 		return new ResponseEntity<ArrayList<Object>>(albums,HttpStatus.OK);
+	}
+	
+	@PostMapping(value="/shared/save", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<Map<String, String>> saveSharedAlbum(@RequestBody Permisos permisos) {
+		
+		permisosRepository.save(permisos);
+		
+		Map<String,String> result = new HashMap<String,String>();
+		result.put("message", "Registro guardado exitosamente");
+				
+		return new ResponseEntity<Map<String,String>>(result,HttpStatus.OK);
 	}
 	
 	public static void main(String[] args) {
--- wchallenge/src/main/resources/static/js/script.js
+++ wchallenge/src/main/resources/static/js/script.js
@@ -1,4 +1,5 @@
 "use strict";
+var urlApi = "http://localhost:8080/api";
 
 function getUsers() {
 
@@ -6,13 +7,13 @@ function getUsers() {
 	btnUsuarios.classList.add("is-loading");
 	btnUsuarios.disabled = true;
 
-	let url = "http://localhost:8080/api/users";
+	let url = urlApi + "/users";
 
 	fetch(url, { method: "GET" })
 		.then(response => response.json())
 		.then(data => {
 			let users = data;
-			var divUsers = "<div class='columns is-multiline'>";
+			let divUsers = "<div class='columns is-multiline'>";
 
 			users.forEach((user) => {
 				divUsers += "<div class='column is-4'>"
@@ -33,16 +34,14 @@ function getUsers() {
 					+ "<p><b>Email: </b>" + user.email + "</p>"
 					+ "<p><b>Phone: </b>" + user.phone + "</p>"
 					+ "<p><b>Website: </b><a>" + user.website + "</a></p>"
-					+ "<div class='columns'>"					
+					+ "<div class='columns'>"
 					+ "<div class='column'><button class='button is-info is-fullwidth btn-modal' onclick='moreInfoUser(" + JSON.stringify(user) + ")'>Más info</button></div>"
-					+ "<div class='column'><button class='button is-warning is-fullwidth' onclick='getAlbums(" + user.id + ")'>Ver albums</button></div>"
+					+ "<div class='column'><button class='button is-warning is-fullwidth' onclick='getAlbums(" + user.id + ")'>Ver álbumes</button></div>"
 					+ "<div class='column'><button class='button is-danger is-fullwidth' onclick='getPhotos(\"user\"," + user.id + ")'>Ver fotos</button></div>"
 					+ "</div>"
-					
-					+ "<div class='columns'>"					
-					+ "<div class='column'><button class='button is-success is-fullwidth' onclick='getSharedAlbums(" + user.id + ")'>Ver álbumes compartidos</button></div>"					
+					+ "<div class='columns'>"
+					+ "<div class='column'><button class='button is-success is-fullwidth' onclick='getSharedAlbums(" + user.id + ")'>Ver álbumes compartidos</button></div>"
 					+ "</div>"
-										
 					+ "</div>"
 					+ "</div>"
 					+ "</div>"
@@ -56,13 +55,13 @@ function getUsers() {
 			btnUsuarios.classList.remove("is-loading");
 			btnUsuarios.disabled = false;
 
-			var btnModal = document.getElementsByClassName('btn-modal');
+			let btnModal = document.getElementsByClassName('btn-modal');
 
-			for (var i = 0; i < btnModal.length; i++) {
+			for (let i = 0; i < btnModal.length; i++) {
 				btnModal[i].addEventListener('click', function(event) {
 					event.preventDefault();
-					var modal = document.querySelector('.modal');  // assuming you have only 1
-					var html = document.querySelector('html');
+					let modal = document.querySelector('.modal');
+					let html = document.querySelector('html');
 					modal.classList.add('is-active');
 					html.classList.add('is-clipped');
 
@@ -73,11 +72,14 @@ function getUsers() {
 					});
 				});
 			}
+		}).catch((e) => {
+			console.log(e.message);
+			alert("Ocurrió un error interno.");
 		});
 }
 
 function moreInfoUser(user) {
-	var divUser = "<div class='columns'><div class='column'>"
+	let divUser = "<div class='columns'><div class='column'>"
 		+ "<div class='card'>"
 		+ "<div class='card-content'>"
 		+ "<div class='media'>"
@@ -119,31 +121,31 @@ function moreInfoUser(user) {
 	document.getElementById("modalContent").innerHTML = divUser;
 }
 
-function getPhotos(tipo,id) {
+function getPhotos(tipo, id) {
 
 	let btnFotos = document.getElementById("btnFotos");
 	btnFotos.classList.add("is-loading");
 	btnFotos.disabled = true;
-	
+
 	let url;
-	
-	if(tipo == "photo"){
-		url = "http://localhost:8080/api/photos";		
+
+	if (tipo == "photo") {
+		url = urlApi + "/photos";
 	} else if (tipo == "album") {
-		url = "http://localhost:8080/api/album/photos?albumId=" + id;		
-	} else if (tipo == "user"){
-		url = "http://localhost:8080/api/user/photos?userId=" + id;
+		url = urlApi + "/album/photos?albumId=" + id;
+	} else if (tipo == "user") {
+		url = urlApi + "/user/photos?userId=" + id;
 	}
-	
+
 	fetch(url, { method: "GET" })
 		.then(response => response.json())
 		.then(data => {
 			let photos = data;
 			let length = (photos.length >= 500 ? 500 : photos.length);
-			var divPhotos = "<div class='columns is-multiline'>";
-			
-			
-			for (var i = 0; i < length; i++) {
+			let divPhotos = "<div class='columns is-multiline'>";
+
+
+			for (let i = 0; i < length; i++) {
 				divPhotos += "<div class='column is-4'>"
 					+ "<div class='card'>"
 					+ "<div class='card-content'>"
@@ -173,13 +175,13 @@ function getPhotos(tipo,id) {
 			btnFotos.classList.remove("is-loading");
 			btnFotos.disabled = false;
 
-			var btnModal = document.getElementsByClassName('img-modal');
+			let btnModal = document.getElementsByClassName('img-modal');
 
-			for (var i = 0; i < btnModal.length; i++) {
+			for (let i = 0; i < btnModal.length; i++) {
 				btnModal[i].addEventListener('click', function(event) {
 					event.preventDefault();
-					var modal = document.querySelector('.modal');  // assuming you have only 1
-					var html = document.querySelector('html');
+					let modal = document.querySelector('.modal');  // assuming you have only 1
+					let html = document.querySelector('html');
 					modal.classList.add('is-active');
 					html.classList.add('is-clipped');
 
@@ -190,11 +192,14 @@ function getPhotos(tipo,id) {
 					});
 				});
 			}
+		}).catch((e) => {
+			console.log(e.message);
+			alert("Ocurrió un error interno.");
 		});
 }
 
 function seePhoto(photo) {
-	var divPhoto = "<p class='image is-4by3'><img src='" + photo.url + "' alt='" + photo.title + "'></p>";
+	let divPhoto = "<p class='image is-4by3'><img src='" + photo.url + "' alt='" + photo.title + "'></p>";
 
 	document.getElementById("modalContent").innerHTML = divPhoto;
 }
@@ -207,84 +212,171 @@ function getAlbums(userId) {
 
 	let url;
 	if (userId == "" || userId == undefined) {
-		url = "http://localhost:8080/api/albums";
+		url = urlApi + "/albums";
 	} else {
-		url = "http://localhost:8080/api/user/album?userId=" + userId;
+		url = urlApi + "/user/album?userId=" + userId;
 	}
-	
 
 	fetch(url, { method: "GET" })
 		.then(response => response.json())
 		.then(data => {
 			let albums = data;
-			var divAlbums = "<div class='columns is-multiline'>";
-
-			albums.forEach((album) => {
-				divAlbums += "<div class='column is-4'>"
-					+ "<div class='card'>"
-					+ "<div class='card-content'>"
-					+ "<div class='media'>"
-					+ "<div class='media-content'>"
-					+ "<p class='title is-4'>ID: " + album.id + "</p>"
-					+ "<p class='subtitle is-6'>" + album.title + "</p>"
-					+ "</div>"
-					+ "</div>"
-					+ "<div class='content'>"
-					+ "<p><b>UserId: </b>" + album.userId + "</p>"
-					+ "<div><button class='button is-info is-fullwidth btn-modal' onclick='getPhotos(\"album\"," + album.id + ")'>Ver fotos</button></div>"
-					+ "</div>"
-					+ "</div>"
-					+ "</div>"
-					+ "</div>";
-			});
-
-			divAlbums += "</div>";
-
-			document.getElementById("content").innerHTML = divAlbums;
+			showAlbums(albums);
 
 			btnAlbumes.classList.remove("is-loading");
 			btnAlbumes.disabled = false;
+		}).catch((e) => {
+			console.log(e.message);
+			alert("Ocurrió un error interno.");
 		});
 }
 
 function getSharedAlbums(userId) {
-	
+
 	let btnAlbumes = document.getElementById("btnAlbumes");
 	btnAlbumes.classList.add("is-loading");
 	btnAlbumes.disabled = true;
 
-	let url = "http://localhost:8080/api/shared/albums?userId=" + userId;
+	let url = urlApi + "/shared/albums?userId=" + userId;
 
 	fetch(url, { method: "GET" })
 		.then(response => response.json())
 		.then(data => {
 			let albums = data;
-			var divAlbums = "<div class='columns is-multiline'>";
-
-			albums.forEach((album) => {
-				divAlbums += "<div class='column is-4'>"
-					+ "<div class='card'>"
-					+ "<div class='card-content'>"
-					+ "<div class='media'>"
-					+ "<div class='media-content'>"
-					+ "<p class='title is-4'>ID: " + album.id + "</p>"
-					+ "<p class='subtitle is-6'>" + album.title + "</p>"
-					+ "</div>"
-					+ "</div>"
-					+ "<div class='content'>"
-					+ "<p><b>UserId: </b>" + album.userId + "</p>"
-					+ "<div><button class='button is-info is-fullwidth btn-modal' onclick='getPhotos(\"album\"," + album.id + ")'>Ver fotos</button></div>"
-					+ "</div>"
-					+ "</div>"
-					+ "</div>"
-					+ "</div>";
-			});
-
-			divAlbums += "</div>";
+			showAlbums(albums);
 
 			document.getElementById("content").innerHTML = divAlbums;
 
 			btnAlbumes.classList.remove("is-loading");
 			btnAlbumes.disabled = false;
+		}).catch((e) => {
+			console.log(e.message);
+			alert("Ocurrió un error interno.");
 		});
+}
+
+function showAlbums(albums) {
+	let divAlbums = "<div class='columns is-multiline'>";
+
+	albums.forEach((album) => {
+		divAlbums += "<div class='column is-4'>"
+			+ "<div class='card'>"
+			+ "<div class='card-content'>"
+			+ "<div class='media'>"
+			+ "<div class='media-content'>"
+			+ "<p class='title is-4'>ID: " + album.id + "</p>"
+			+ "<p class='subtitle is-6'>" + album.title + "</p>"
+			+ "</div>"
+			+ "</div>"
+			+ "<div class='content'>"
+			+ "<p><b>UserId: </b>" + album.userId + "</p>"
+			+ "<div class='columns'>"
+			+ "<div class='column'><button class='button is-info is-fullwidth' onclick='getPhotos(\"album\"," + album.id + ")'>Ver fotos</button></div>"
+			+ "<div class='column'><button class='button is-warning is-fullwidth btn-modal' onclick='shareAlbum(" + JSON.stringify(album) + ")'>Compartir álbum</button></div>"
+			+ "</div>"
+			+ "</div>"
+			+ "</div>"
+			+ "</div>"
+			+ "</div>";
+	});
+
+	divAlbums += "</div>";
+
+	document.getElementById("content").innerHTML = divAlbums;
+
+	let btnModal = document.getElementsByClassName('btn-modal');
+
+	for (let i = 0; i < btnModal.length; i++) {
+		btnModal[i].addEventListener('click', function(event) {
+			event.preventDefault();
+			let modal = document.querySelector('.modal');
+			let html = document.querySelector('html');
+			modal.classList.add('is-active');
+			html.classList.add('is-clipped');
+
+			modal.querySelector('.modal-background').addEventListener('click', function(e) {
+				e.preventDefault();
+				modal.classList.remove('is-active');
+				html.classList.remove('is-clipped');
+			});
+		});
+	}
+}
+
+function shareAlbum(album) {
+	let divUser =
+		"<div class='card'>"
+		+ "<div class='card-content'>"
+		+ "<div class='media'>"
+		+ "<div class='media-content'>"
+		+ "<p class='title is-4'>ID: " + album.id + "</p>"
+		+ "<p class='subtitle is-6'>" + album.title + "</p>"
+		+ "</div>"
+		+ "</div>"
+		+ "<p>Compartir con:</p>"
+		+ "<div class='columns'>"
+		+ "<div class='column'>"
+		+ "<label class='label'>UserId: </label>"
+		+ "<input id='inputUserId' class='input' type='number' placeholder='User id'>"
+		+ "</div>"
+		+ "<div class='column'>"
+		+ "<label class='label'>Permisos: </label>"
+		+ "<div class='columns'>"
+		+ "<div class='column'>"
+		+ "<label class='checkbox'><input id='checkLectura' type='checkbox'> Lectura</label>"
+		+ "</div>"
+		+ "<div class='column'>"
+		+ "<label class='checkbox'><input id='checkEscritura' type='checkbox'> Escritura</label>"
+		+ "</div>"
+		+ "</div>"
+		+ "</div>"
+		+ "<div class='column'>"
+		+ "<button id='btnCompartir' class='button is-info is-success is-fullwidth btn-modal btn-compartir' onclick='saveSharedAlbum(" + album.id + ")'>Guardar</button>"
+		+ "</div>"
+		+ "</div>"
+		+ "</div>"
+		+ "</div>";
+
+	document.getElementById("modalContent").innerHTML = divUser;
+}
+
+function saveSharedAlbum(albumId) {
+	let btnCompartir = document.getElementById("btnCompartir");
+	btnCompartir.classList.add("is-loading");
+	btnCompartir.disabled = true;
+
+	let permisos = {
+		userId: document.getElementById("inputUserId").value,
+		albumId: albumId,
+		lectura: document.getElementById("checkLectura").checked,
+		escritura: document.getElementById("checkEscritura").checked
+	}
+
+	let url = urlApi + "/shared/save";
+
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(permisos)
+	})
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+
+			alert(data.message);
+
+			document.getElementById("inputUserId").value = "";
+			document.getElementById("checkLectura").checked = false;
+			document.getElementById("checkEscritura").checked = false;
+
+			btnCompartir.classList.remove("is-loading");
+			btnCompartir.disabled = false;
+		}).catch((e) => {
+			console.log(e.message);
+			alert("Ocurrió un error interno.");
+		});
+
 }
